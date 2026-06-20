920c3216b821da34a167360475d4bef48dbf057f
Yogeshwari0511 <yogeshwari40021105@gmail.com>
2026-06-20 09:13:56 +0530

    program3

---
+++ Square.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Square {
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    double a=sc.nextDouble();
+    System.out.println("a:");
+
+    double area=a*a;
+    System.out.println("Area");
+  }
+
+}
