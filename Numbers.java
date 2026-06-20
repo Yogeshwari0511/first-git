7e5944f48d60192cd0719eae3b85f17952066432
Yogeshwari0511 <yogeshwari40021105@gmail.com>
2026-06-20 06:36:27 +0530

    Swapnumbers

---
+++ Numbers.java
@@ -0,0 +1,10 @@
+class Numbers{
+    public static void main(String[]args){
+
+        int a=10,b=5,temp;
+        temp=a;
+        b=a;
+        b=temp;
+        System.out.println(a+" "+b);
+    }
+}
